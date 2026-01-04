21e4619d204fdfd80bb117c23ae279a50ad1816d
Apurba Dutta <bestytheapurva@gmail.com>
2026-01-04 18:26:55 +0530

    deploy.yml

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import path from 'path'
 
 export default defineConfig({
     plugins: [react()],
+    base: process.env.GITHUB_REPOSITORY 
+        ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/`
+        : '/',
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
